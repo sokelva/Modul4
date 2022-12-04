0ef4a623f8d0cf7d6d437f4d736255262e54986b
sokelva <sokelva@mail.ru>
2022-12-04 15:32:26 +0300

    Задание 4.1.5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,9 +11,16 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            string A= "--";
-            string B = "234";
-            bool C = A != B;
+            //string A= "--";
+            //string B = "234";
+            //bool C = A != B;
+
+            int A = 5;
+            int B = 3;
+            double X = 5;
+            double Y = 4;
+
+            bool C = (A < B) | (X > Y);
 
             Console.WriteLine(C);
             Console.ReadKey();

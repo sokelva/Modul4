5929a2e17b938bd06e0d34cdd3a9171ffd540717
sokelva <sokelva@mail.ru>
2022-12-04 19:45:11 +0300

    Задание 4.3.13

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -338,27 +338,38 @@ namespace ConsoleApp1
             //}
             //Console.ReadKey();
 
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            //var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            int temp;
+            //int temp;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] > arr[j])
+            //        {
+            //            temp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = temp;
+            //        }
+            //    }
+            //}
+
+            //foreach (var item in arr)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.ReadKey();
+            //--------------------------------------------------------------------------
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int sum = 0;
 
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
+                sum += arr[i];
             }
 
-            foreach (var item in arr)
-            {
-                Console.Write(item);
-            }
+            Console.WriteLine(sum);
             Console.ReadKey();
         }
     

d593c098a9a9819eb3007cce0517b0981cefa9b6
sokelva <sokelva@mail.ru>
2022-12-04 19:51:51 +0300

    Задание 4.3.14

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -361,15 +361,30 @@ namespace ConsoleApp1
             //}
             //Console.ReadKey();
             //--------------------------------------------------------------------------
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int sum = 0;
+            //var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            //int sum = 0;
 
-            for (int i = 0; i < arr.Length; i++)
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum += arr[i];
+            //}
+
+            //Console.WriteLine(sum);
+
+            //---------Зубчатый массив
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach (var num in array)
             {
-                sum += arr[i];
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
             }
-
-            Console.WriteLine(sum);
             Console.ReadKey();
         }
     

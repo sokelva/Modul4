d5a7704951bd70c69a780dc8443afba8fb2376ea
sokelva <sokelva@mail.ru>
2022-12-04 15:27:51 +0300

    Задание 4.1.4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            string A= "--";
+            string B = "234";
+            bool C = A != B;
 
+            Console.WriteLine(C);
+            Console.ReadKey();
 
         }
     }
